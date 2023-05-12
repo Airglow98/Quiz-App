c0098a74a8c5fc4159d1a57bac0927e2a8668fed
Niklas Wieland <niklaswielandhotmail.de>
2023-05-12 16:12:01 +0200

    add feature newquestions

--- form.js
+++ form.js
@@ -3,12 +3,47 @@ const formElement = document.querySelector('[data-js="form"]');
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    const questionTextInput= data.yourQuestionTextarea;
+    const answerTextInput = data.yourAnswerTextarea;
+    const tag= data.tag;
+
     event.target.reset();
-   alert("Thanks for your question");
+
+
+   const quizQuestionBlock = document.createElement('div');
+   const quizQuestion = document.createElement('p');
+   const quizQuestionAnswer = document.createElement('p');
+   const quizQuestionAnswerButton= document.createElement('button');
+   const quizQuestionTagUl = document.createElement('ul');
+   const quizQuestionTag = document.createElement('li');
+ 
+
+quizQuestionBlock.classList.add('music-card');
+quizQuestionAnswer.classList.add('hidden__answer');
+quizQuestionAnswerButton.classList.add('quiz-card__answerbutton');
+quizQuestionTagUl.classList.add('quiz-card__tag');
+quizQuestionTag.classList.add('quiz-card__musik-tag');
+
+
+
+   quizQuestion.textContent = questionTextInput;
+   quizQuestionAnswer.textContent = answerTextInput;
+   quizQuestionAnswerButton.textContent = "Show Answer";
+   quizQuestionTag.textContent = tag;
+
+
+
+
+
+   document.body.append(quizQuestionBlock);
+   quizQuestionBlock.append(quizQuestion);
+   quizQuestionBlock.append(quizQuestionAnswer);
+   quizQuestionBlock.append(quizQuestionAnswerButton);
+   quizQuestionBlock.append(quizQuestionTagUl);
+   quizQuestionTagUl.append(quizQuestionTag);
+ 
     });
 
-    const quizQuestionBlock = document.createElement('div');
-    const quizQuestion = document.createElement('p');
-    const quizQuestionAnswerButton= document.createElement('button');
-    const quizQuestionTag = document.createElement('li');
-    const quizQuestionFavouriteButton = document.createElement('svg');
+   
