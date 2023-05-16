a8d386d5c2bac0adc56c12eb350d8de2510524a6
Niklas Wieland <niklaswielandhotmail.de>
2023-05-16 16:44:09 +0200

    add forEach function

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const favouriteButton = document.querySelector('[data-js="favouritebutton"]');
-const svg = document.querySelector('[data-js="svg"]');
-const answerButton = document.querySelector('[data-js="answerButton"]');
-const hiddenAnswer = document.querySelector('[data-js="hiddenAnswer"]');
-/* const formSubmitButton = document.querySelector('[data-js="form-submitbutton"]') */
-
-favouriteButton.addEventListener ("click", () => {
-    svg.classList.toggle("quiz-card__bookmarked");
-});
 
 
-  
+const quizCards = document.querySelectorAll('[data-js="quizCard"]');
+
+
+
+
+
+  quizCards.forEach(quizCard => {
+    const answerButton = quizCard.querySelector('[data-js="answerButton"]');
+    const hiddenAnswer = quizCard.querySelector('[data-js="hiddenAnswer"]');
+    const favouriteButton = quizCard.querySelector('[data-js="favouritebutton"]');
+    const svg = quizCard.querySelector('[data-js="svg"]');
+
     answerButton.addEventListener("click", () => {
       if (answerButton.innerText === "Show Answer") {
         answerButton.textContent = "Hide Answer";
@@ -17,29 +19,10 @@ favouriteButton.addEventListener ("click", () => {
         answerButton.textContent = "Show Answer";
       }
       hiddenAnswer.classList.toggle("not-hidden__answer");
-    
     });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const darkModeButton = document.querySelector('[data-js="darkModeButton"]');
-const htmlGoesDark = document.querySelector('[data-js="htmlGoesDark"]');
-
-console.log(darkModeButton)
-
-/darkModeButton.addEventListener ("click", () => {
-    htmlGoesDark.classList.toggle("darkmode");
-});
- */
+      favouriteButton.addEventListener ("click", () => {
+        svg.classList.toggle("quiz-card__bookmarked");
+   
+    });
+    });
